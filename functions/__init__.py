a7e92c27c511f834cda867f5395dcef968845080
star14ms <68639278+star14ms@users.noreply.github.com>
2020-08-19 21:02:32 +0900

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,4 +1,4 @@
 from .change_str_num import *
 from .change_to_decimal import *
+from .change_to_x_expression import *
 from .isTrue_rational_num import *
-from .print_human_tailored_expansion import *
